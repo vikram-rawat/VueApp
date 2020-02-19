77dabb2efee78e57883e7e5a0225885cef15bf29
vasu_dargar <vasudargar@gmail.com>
2020-02-19 16:43:48 +0530

    code cleaning

--- Route.R
+++ Route.R
@@ -4,14 +4,16 @@
 route <- Route$new()
 router <- RouteStack$new()
 
-# hello Path --------------------------------------------------------------
+# home Path --------------------------------------------------------------
 
 route$add_handler('get', '/helloworld', home)
 
 route$add_handler('get', '/', homeFallback)
-static_route <- ressource_route('/dist' = "vue-app/dist/")
+static_route <- ressource_route('/dist' = "vue-app/dist/",
+                                default_file = "vue-app/dist/css/app.fb0c6e1c.css",
+                                default_ext = ".css")
 
 # create router -----------------------------------------------------------
 
-router$add_route(route, 'test')
+router$add_route(route, 'home')
 router$add_route(static_route, 'static', after = 1)
