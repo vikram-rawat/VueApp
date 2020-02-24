edc05e6ff0ad16f48e4057504575199adc565cce
vasu_dargar <vasudargar@gmail.com>
2020-02-24 15:34:48 +0530

    added plotly support

---
+++ fiery-app/pages/Plotly.vue
@@ -0,0 +1,23 @@
+<template>
+    <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
+</template>
+
+<script>
+import { Plotly } from 'vue-plotly'
+
+export default {
+  components: {
+    Plotly
+  },
+  data:{
+    data:[{
+      x: [1,2,3,4],
+      y: [10,15,13,17],
+      type:"scatter"
+    }],
+    layout:{
+      title: "My graph"
+    }
+  }
+}
+</script> 
